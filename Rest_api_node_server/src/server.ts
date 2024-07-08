e539803e3bd941d1cfb2dbc4b5b519d045440a00
gabicilfone <cilfonegabriel@gmail.com>
2024-07-08 18:20:53 -0300

    Conectando la app a ladb

--- Rest_api_node_server/src/server.ts
+++ Rest_api_node_server/src/server.ts
@@ -1,5 +1,20 @@
 import express from 'express'
 import router from './router'
+import db from './config/db'
+
+// Conectar a base de datos
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('Conexión exitosa a la DB')
+    } catch (error) {
+        console.log(error)
+        console.log("Hubo un error al conectar la base de datos")
+    }
+}
+
+connectDB()
 
 const server = express()
 
