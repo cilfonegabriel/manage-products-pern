6c383c8d4049936e7665997a8324ccb28912f233
gabicilfone <cilfonegabriel@gmail.com>
2024-07-11 08:22:58 -0300

    Create an Endpointo get all products

---
+++ src/handlers/product.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express"
+import Product from "../models/Product.model";
+
+export const getProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await Product.findAll({
+            order: [
+                ['price', 'DESC']
+            ]
+        })
+        res.json({data: products})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const createProduct = async (req: Request, res: Response) => {
+    try {
+        const product = await Product.create(req.body)
+        res.json({data: product});
+    } catch (error) {
+        console.log(error);
+    }
+}
