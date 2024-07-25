1290a13e832e924610391f0f79d1ee7cee659d3b
gabicilfone <cilfonegabriel@gmail.com>
2024-07-25 09:51:44 -0300

    Add test that the price is a number and greater than 0

--- Rest_api_node_server/src/handlers/__tests__/product.test.ts
+++ Rest_api_node_server/src/handlers/__tests__/product.test.ts
@@ -27,4 +27,32 @@ describe('POST /api/products', () => {
         expect(response.status).not.toBe(200)
         expect(response.body).not.toHaveProperty('error')
     })
+
+    it('should validate that the price is greater than 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: "Monitor curvo",
+            price: 0
+        })
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.status).not.toBe(201);
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+    it('should validate that the price is a number and greater than 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: "Monitor curvo",
+            price: "hola"
+        })
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+
+        expect(response.status).not.toBe(201);
+        expect(response.body.errors).not.toHaveLength(3)
+    })
 })
