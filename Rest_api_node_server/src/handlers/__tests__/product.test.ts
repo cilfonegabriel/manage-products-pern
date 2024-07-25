1d431cc3b2245620c25f4eeab688937ca772970b
gabicilfone <cilfonegabriel@gmail.com>
2024-07-25 09:41:55 -0300

    Add testing  create product validation

--- Rest_api_node_server/src/handlers/__tests__/product.test.ts
+++ Rest_api_node_server/src/handlers/__tests__/product.test.ts
@@ -2,6 +2,18 @@ import request from "supertest";
 import server from "../../server";
 
 describe('POST /api/products', () => {
+    it('should display validation errors', async () => {
+        const response = await request(server).post('/api/products').send({
+
+        })
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(201);
+        expect(response.body.errors).not.toHaveLength(3)
+    })
+
     it('should create a new product', async () => {
         const response = await request(server).post('/api/products').send({
             name: "Mouse -Testing",
@@ -13,6 +25,6 @@ describe('POST /api/products', () => {
 
         expect(response.status).not.toBe(404)
         expect(response.status).not.toBe(200)
-        expect(response.body).toHaveProperty('error')
+        expect(response.body).not.toHaveProperty('error')
     })
 })
