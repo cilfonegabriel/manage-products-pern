b9117e0de1ac0bf770fc5cc6dce26c287431ab40
gabicilfone <cilfonegabriel@gmail.com>
2024-08-07 18:16:12 -0300

    Documenting Create Product

--- Rest_api_node_server/src/router.ts
+++ Rest_api_node_server/src/router.ts
@@ -86,6 +86,35 @@ router.get('/:id',
     getProductById 
 );
 
+/**
+ * @swagger
+ * /api/products:
+ *      post:
+ *          summary: Create a new product
+ *          tags:
+ *              - Products
+ *          description: return a new record in the database
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema: 
+ *                          type: object
+ *                          properties:
+ *                              name:
+ *                                  type: string
+ *                                  example: 'Monitor Curvo 49 pulgadas'
+ *                              price:
+ *                                  type: number
+ *                                  example: 399
+ * 
+ *          responses:
+ *              201:
+ *                  description: Product created successfully
+ *              400:
+ *                  description: Bad Request - invalid input data
+ */
+
 router.post('/', 
     // Validación
     body('name')
